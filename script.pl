5307f4b0f735a784ec2363da7d62acd8edd55940
vlad <vlastachu@gmail.com>
2015-08-26 17:52:02 +0300

    experiments. Stupid commit, remove me

---
+++ script.pl
@@ -0,0 +1,11 @@
+use strict;
+use warnings;
+ 
+my $filename = 'data.txt';
+open(my $fh, '<:encoding(UTF-8)', $filename)
+  or die "Could not open file '$filename' $!";
+ 
+while (my $row = <$fh>) {
+  chomp $row;
+  print "$row\n";
+}
